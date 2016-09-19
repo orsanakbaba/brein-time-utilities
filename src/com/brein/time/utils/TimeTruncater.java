2fc2e2e9073fe2d39b11688675daf1cbc27c8752
Philipp Meisen <philipp@meisen.net>
2016-09-19 10:31:28 -0700

    formatting

--- src/com/brein/time/utils/TimeTruncater.java
+++ src/com/brein/time/utils/TimeTruncater.java
@@ -9,6 +9,22 @@ public class TimeTruncater {
     private static final ZoneId UTC = ZoneId.of("UTC");
 
     /**
+     * Truncates the passed time-stamp to the first of the month information only, i.e., the 09/10/2016 00:14:43 will be
+     * truncated to 01/10/2016 00:00:00.
+     *
+     * @param unixTimeStamp the time-stamp to be truncated
+     *
+     * @return the date information only
+     */
+    public static long toMonth(final long unixTimeStamp) {
+        final Instant instant = Instant.ofEpochSecond(unixTimeStamp);
+        final ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(instant, UTC);
+        return zonedDateTime.withDayOfMonth(1)
+                .with(LocalTime.of(0, 0))
+                .toEpochSecond();
+    }
+
+    /**
      * Truncates the passed time-stamp to the date information only, i.e., the 09/10/2016 00:14:43 will be truncated to
      * 09/10/2016 00:00:00.
      *
@@ -36,25 +52,17 @@ public class TimeTruncater {
         return zonedDateTime.with(LocalTime.of(zonedDateTime.getHour(), 0)).toEpochSecond();
     }
 
+    /**
+     * Truncates the second from the passed time-stamp, i.e., the 09/10/2016 02:14:43 will be truncated to
+     * 09/10/2016 02:14:00.
+     *
+     * @param unixTimeStamp the time-stamp to be truncated
+     *
+     * @return the truncated time-stamp
+     */
     public static long toMinute(final long unixTimeStamp) {
         final Instant instant = Instant.ofEpochSecond(unixTimeStamp);
         final ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(instant, UTC);
         return zonedDateTime.with(LocalTime.of(zonedDateTime.getHour(), zonedDateTime.getMinute())).toEpochSecond();
-    }
-
-    /**
-     * Truncates the passed time-stamp to the first of the month information only, i.e., the 09/10/2016 00:14:43 will be
-     * truncated to 01/10/2016 00:00:00.
-     *
-     * @param unixTimeStamp the time-stamp to be truncated
-     *
-     * @return the date information only
-     */
-    public static long toMonth(final long unixTimeStamp) {
-        final Instant instant = Instant.ofEpochSecond(unixTimeStamp);
-        final ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(instant, UTC);
-        return zonedDateTime.withDayOfMonth(1)
-                .with(LocalTime.of(0, 0))
-                .toEpochSecond();
     }
 }
