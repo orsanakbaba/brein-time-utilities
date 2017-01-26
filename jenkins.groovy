1091394790008de2da7f0c9589af74eb55a22359
Philipp Meisen <philipp@meisen.net>
2017-01-25 18:04:17 -0800

    updated jenkins build

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,37 +1,6 @@
-node('master') {
-
-    // we need the new versions
-    stage ('Checkout') {
-        dir('brein-workspace') {
-            git credentialsId: 'BREIN-STAGE-ACCESS', url: 'https://github.com/Breinify/brein-workspace.git'
-        }
-
-        dir ('brein-time-utilities') {
-            git url: 'https://github.com/Breinify/brein-time-utilities.git'
-        }
-    }
-
-    // it is enough to run the test, it will resolve, build and test
-    stage ('Test & Build') {
-        try {
-            dir ('brein-time-utilities') {
-                sh 'ant 06-run-test-suite'
-            }
-        } catch (err) {
-            emailextrecipients([[$class: 'CulpritsRecipientProvider']])
-            throw err
-        }
-    }
-
-    // now we should just publish the new version, it made it through the test
-    stage ('Deploy') {
-        try {
-            dir ('brein-time-utilities') {
-                sh 'ant 04-publish-results'
-            }
-        } catch (err) {
-            emailextrecipients([[$class: 'CulpritsRecipientProvider']])
-            throw err
-        }
-    }
+def execute(String moduleDir, Object master) {
+    master.defaultJavaPipeline(moduleDir)
 }
+
+// do not forget this line, otherwise the script cannot be executed
+return this
