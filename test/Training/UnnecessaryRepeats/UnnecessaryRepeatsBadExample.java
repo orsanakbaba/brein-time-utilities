1e974151b8c6b2855e8f9f7b6cec0bc9b9592eab
msongur <msongur@havelsan.com.tr>
2020-02-24 08:59:53 +0300

    Students branch completed.

--- test/Training/UnnecessaryRepeats/UnnecessaryRepeatsBadExample.java
+++ test/Training/UnnecessaryRepeats/UnnecessaryRepeatsBadExample.java
@@ -10,25 +10,21 @@ import java.time.format.DateTimeFormatter;
 public class UnnecessaryRepeatsBadExample {
    @Test
     public void testDateStringPattern1() {
-       DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-       Assert.assertEquals(1480447581L, TimeUtils.dateStringToUnixTimestamp("2016-11-30 04:26:21", formatter, "Asia/Seoul"));
+
    }
 
     @Test
     public void testDateStringPattern2() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-M-d H:m:s");
-        Assert.assertEquals(1492348192L, TimeUtils.dateStringToUnixTimestamp("2017-04-16 8:09:52", formatter, "America/Chicago"));
+
     }
 
     @Test
     public void testDateStringPattern3() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        Assert.assertEquals(1480447581L, TimeUtils.dateStringToUnixTimestamp("30-11-2016 04:26:21", formatter, "Asia/Seoul"));
+
     }
 
     @Test
     public void testDateStringPattern4() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
-        Assert.assertEquals(1480447581L, TimeUtils.dateStringToUnixTimestamp("11-30-2016 04:26:21", formatter, "Asia/Seoul"));
+
     }
 }
--- test/Training/UnnecessaryRepeats/UnnecessaryRepeatsGoodExample.java
+++ test/Training/UnnecessaryRepeats/UnnecessaryRepeatsGoodExample.java
@@ -10,12 +10,12 @@ public class UnnecessaryRepeatsGoodExample {
 
     @Test
     public void testDateStringPattern1() {
-        Assert.assertEquals(1480447581L, TimeUtils.dateStringToUnixTimestamp("2016-11-30 04:26:21", getFormat("yyyy-MM-dd HH:mm:ss"), "Asia/Seoul"));
+
     }
 
     @Test
     public void testDateStringPattern2() {
-        Assert.assertEquals(1492348192L, TimeUtils.dateStringToUnixTimestamp("2017-04-16 8:09:52", getFormat("yyyy-M-d H:m:s"), "America/Chicago"));
+
     }
 
 
