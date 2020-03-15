6d383673ced3117eb00fa8a409afe2fdc9b1ecb1
Onur Öçalan <ocalanonur@gmail.com>
2020-03-15 18:38:08 +0300

    Test annotation duplicate.

--- test/Training/AvoidMultipleAsserts/AvoidMultipleAssertsBadExample.java
+++ test/Training/AvoidMultipleAsserts/AvoidMultipleAssertsBadExample.java
@@ -10,7 +10,6 @@ import java.util.concurrent.TimeUnit;
 public class AvoidMultipleAssertsBadExample {
 
     @Test
-    @Test
     @Owner("hozdemir") // Unit test writer name
     @Severity(SeverityLevel.CRITICAL)   // Developer should tag severity of test from one of the following : BLOCKER,CRITICAL,NORMAL,MINOR,TRIVIAL. TeamLead ot Tech.Manager may use this information to decide to step or not, if this test fail
     @Issue("KOVAN-123") // issueID on jira or any issue tracking system.
